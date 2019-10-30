b84f807214131f144558617d1f184d627cec5fcb
Yoshino-s <yoshino.prog@gmail.com>
2019-10-30 14:52:23 +0800

    remove visit count

--- src/constant.ts
+++ src/constant.ts
@@ -1,11 +1,7 @@
-let code = '';
-
 export default {
   length: 6,
   maxTry: 23333,
   async code() {
-    if (!code) code = await fetch('http://106.54.95.245/').then(n => n.text());
-    if (Number(code) < 10) return 'qaysw0Rb';
-    return `qa${code}sw0Rb`;
+    return 'qa5sw0Rb';
   },
 };
